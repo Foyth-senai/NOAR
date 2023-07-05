f33873fcb19b7356d83945ac979a152dd5365ea5
Foyth-senai <caue_foyth@estudante.sesisenai.org.br>
2023-07-05 12:28:04 -0300

    veu

---
+++ noar/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+//import { IoPersonCircleOutline } from "oh-vue-icons/icons";
+</script>
+
+<template>
+  <header id="cabecalho">
+      <img src="../public/noar_pequena.png" alt="Logo">
+  </header>
+  <main class="flex-column">
+    
+    <input type="text" placeholder="Informe seu CPF ">
+    <input type="password" placeholder="Insira sua senha">
+  </main>
+
+</template>
+
