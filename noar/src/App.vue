092e5e995e13f76084beb0965e11716c4e778211
Foyth-senai <caue_foyth@estudante.sesisenai.org.br>
2023-07-06 09:50:19 -0300

    Avanços

--- noar/src/App.vue
+++ noar/src/App.vue
@@ -5,14 +5,29 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header id="cabecalho">
-      <img src="../public/noar_pequena.png" alt="Logo">
-  </header>
   <main class="flex-column">
-    
-    <input type="text" placeholder="Informe seu CPF ">
-    <input type="password" placeholder="Insira sua senha">
-  </main>
+    <div class="glass">
+      <header id="cabecalho">
+        <img src="../public/noar_pequena.png" alt="Logo">
+      </header>
 
+      <form action="" class="meio">
+        <div class="container">
+          <input required="" type="text" name="text" class="input">
+          <label class="label">Insira seu CPF</label>
+        </div>
+        <div class="container">
+          <input required="" type="password" name="text" class="input">
+          <label class="label">Insira sua senha</label>
+        </div>
+
+        <button type="subimt" class="button"> Entrar </button>
+        <a href="https://css.glass/">
+          <p class="texto">Esqueceu sua senha?</p>
+        </a>
+      </form>
+
+    </div>
+  </main>
 </template>
 
